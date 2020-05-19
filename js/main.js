3fca5769f173f3928998180b9b5ee7f4a30a7df4
Lukanin Mikhail <mlukanin@ics.perm.ru>
2020-05-19 23:19:06 +0500

    Обновление 0.5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-    $(".dropdown-trigger").dropdown({
-        constrainWidth: false
-    });
+    
+
+    function initApp(){
+        $('.dynamic-dropdown-content').html('<li><a href="listform.html">Списковая форма</a></li>'
+        +'<li><a href="editform.html">Форма редактирования</a></li>'
+        +'<li><a href="login.html">Логин</a></li>'
+        +'<li class="divider"></li>');
+
+
+        // ИНИЦИАЛИЗАЦИЯ ЭЛЕМЕНТОВ MATERIALIZE
+        $(".dropdown-trigger").dropdown({
+            constrainWidth: false
+        });
+    }
+
+    initApp();
 });
